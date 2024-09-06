7dc9424b91281c357536716d328f71c35dd1d478
Gabriel Finetto <gabriel-finetto@hotmail.com>
2024-09-06 13:07:51 -0300

    Aula 02 - Tipo de dados no javascript

---
+++ Aula02/tipo-null-undefined.js
@@ -0,0 +1,10 @@
+let nomeEtudante;
+const professora = 'Ana';
+
+console.log(nomeEtudante);
+console.log(typeof nomeEtudante);
+console.log(typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante); //Por um erro durante a construção do javascript variaveis do tipo nulo sempre retornarão como object. 
