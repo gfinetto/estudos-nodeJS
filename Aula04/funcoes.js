e359c9ee7aeb259a7ad0ede109f556b94534ea84
Gabriel Finetto <gabriel-finetto@hotmail.com>
2024-09-17 01:57:42 -0300

    Aula 04 - Node Alura

---
+++ Aula04/funcoes.js
@@ -0,0 +1,9 @@
+//parâmetros/argumentos 
+//retorno
+
+function exibeInfosEstudante(nome, nota) { //Os parâmentros são incluidos nos parentêses eles podem ser vázios (sem parâmetros), ou com um ou mais valores. 
+   return `O nome é ${nome} e a nota é ${nota}`;
+}
+
+console.log(exibeInfosEstudante('João', 7));
+console.log(exibeInfosEstudante('Maria', 4));
